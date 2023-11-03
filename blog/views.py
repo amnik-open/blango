c634da744c1cb0b0acfb45c91c93d6698cbef532
Amir Nikpour <amnik_75@yahoo.com>
2023-11-03 19:38:21 +0000

    Finish JavaScript intro and fundamentals

--- blog/views.py
+++ blog/views.py
@@ -40,3 +40,6 @@ def index(request):
 def get_ip(request):
   from django.http import HttpResponse
   return HttpResponse(request.META['REMOTE_ADDR'])
+
+def post_table(request):
+    return render(request, "blog/post-table.html")
